9454f70a3ae54d37295b6cc13c992b09fdc7c207
kashyap <51871002+kashyap-hindocha@users.noreply.github.com>
2019-09-27 03:59:26 -0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,70 @@
 <?php
 
-Route::get('welcome', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::get('/about','about@about');
-
-// Route::post('/register','register@register');
-// Route::post('/login','LoginController@Login');
-
-Route::get('/login',[
-	'as' => 'user.login',
+ Route::resource('product','gallary');
+Route::get('/',[
+    'as' => 'user.login',
     'uses' => 'LoginController@login']);
-    Route::post('/loginsuccess',[
+Route::get('/register','LoginController@register');
+Route::post('/loginsuccess',[
     'as' => 'login.success',
-    'uses' => 'logincontroller@loginsuccess']);
+    'uses' => 'LoginController@loginsuccess']);
 
+Route::get('/user_add','LoginController@add_janu');
+Route::get('/data','AdminLogin@data');
+
+Route::post('/adduser','LoginController@success');
+Route::get('/admin','AdminLogin@index');
+Route::post('/check','AdminLogin@check'); 
+Route::get('/admin_user','logincontroller@user');
+Route::get('/add_product','ProductController@add_product');
+Route::get('/add_p','ProductController@index');
+Route::get('delete/{course_id}', [
+        'as' => 'course.delete',
+        'uses' => 'AdminLogin@delete'
+ ]);
+Route::post('/add_register','logincontroller@success');
+
+
+Route::middleware('auth')
+    ->group(function () 
+{
+    Route::get('/about', function () {
+        return view('about');
+    });
+    Route::get('index', function () {
+        return view('index');
+    });
+    
+    Route::get('/codes', function () {
+        return view('codes');
+    });
+    Route::get('/contact', function () {
+        return view('contact');
+    });
+    
+    Route::post('/add','logincontroller@add');
+    
+    
     Route::get('logout', [
         'as' => 'logout',
         'uses' => 'logincontroller@logout',]);
+    
+    
+    Route::resource('feedback','feedbackController');
+    Route::resource('contact','contactcontroller');
 
-        Route::get('/home',function(){
-            return view('home');
-        });
 
-// Route::get('/data','AdminLogin@data');
-Route::post('/adduser','logincontroller@success');
-// Route::get('/admin','AdminLogin@index');
-// Route::post('/check','AdminLogin@check'); 
-// Route::get('/admin_user','logincontroller@user');
-// Route::get('/add_product','ProductController@add_product');
-// Route::get('/add_p','ProductController@index');
-// Route::get('delete/{course_id}', [
-//         'as' => 'course.delete',
-//         'uses' => 'AdminLogin@delete'
- //]);
-Route::get('/single','products@single');
-Route::get('/single2','products@single2');
-Route::get('/products','products@products');
-Route::get('/products2','products@products2');
-
-//Auth::routes('auth.login')->name('auth');
-
-Route::get('/home', 'HomeController@index')->name('home');
+    
+    
+});
