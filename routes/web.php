d518c8451a9d5728ee9c8707d3638cff7cabb9a4
kashyap <51871002+kashyap-hindocha@users.noreply.github.com>
2019-10-09 08:58:36 -0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -11,60 +11,121 @@
 |
 */
 
- Route::resource('product','gallary');
-Route::get('/',[
-    'as' => 'user.login',
-    'uses' => 'LoginController@login']);
-Route::get('/register','LoginController@register');
-Route::post('/loginsuccess',[
-    'as' => 'login.success',
-    'uses' => 'LoginController@loginsuccess']);
-
-Route::get('/user_add','LoginController@add_janu');
-Route::get('/data','AdminLogin@data');
-
-Route::post('/adduser','LoginController@success');
-Route::get('/admin','AdminLogin@index');
-Route::post('/check','AdminLogin@check'); 
-Route::get('/admin_user','logincontroller@user');
-Route::get('/add_product','ProductController@add_product');
-Route::get('/add_p','ProductController@index');
-Route::get('delete/{course_id}', [
-        'as' => 'course.delete',
-        'uses' => 'AdminLogin@delete'
- ]);
-Route::post('/add_register','logincontroller@success');
-
-
-Route::middleware('auth')
-    ->group(function () 
-{
-    Route::get('/about', function () {
-        return view('about');
-    });
-    Route::get('index', function () {
-        return view('index');
-    });
-    
-    Route::get('/codes', function () {
-        return view('codes');
-    });
-    Route::get('/contact', function () {
-        return view('contact');
-    });
-    
-    Route::post('/add','logincontroller@add');
-    
-    
-    Route::get('logout', [
-        'as' => 'logout',
-        'uses' => 'logincontroller@logout',]);
-    
-    
-    Route::resource('feedback','feedbackController');
-    Route::resource('contact','contactcontroller');
-
-
-    
-    
+Route::get('/', function () {
+    return view('welcome');
 });
+Route::match(['get','post'],'/admin', 'AdminController@login'); //submit our form for login process
+
+Auth::routes();
+//Home Page Route
+Route::get('/home', 'HomeController@index')->name('home');
+
+//Index page
+Route::get('/','IndexController@index');
+
+//Contact Us page Route
+Route::get('/contact-us','IndexController@contactUs');
+
+//Category listing page  
+Route::get('/products/{url}','ProductsController@products');
+
+//Product Detail Page
+Route::get('product/{id}','ProductsController@product');
+
+//Cart Page Route
+Route::match(['get','post'],'/cart', 'ProductsController@cart');
+
+//Add to cart Route
+Route::match(['get','post'],'/add-cart', 'ProductsController@addtocart');
+
+//Delete Cart items Route
+Route::get('cart/delete-product/{id}','ProductsController@deleteCartProduct');
+
+//Update product quantity in cart
+Route::get('/cart/update-quantity/{id}/{quantity}','ProductsController@updateCartQuantity');
+
+//Get product Attribute Price
+Route::get('/get-product-price','ProductsController@getProductPrize');
+
+//Apply Coupon Code
+Route::post('/cart/apply-coupon','ProductsController@ApplyCoupon');
+
+// User Login/Register Route
+Route::get('/login-register','UsersController@userLoginRegister');
+// User register form submit
+Route::post('/user-register','UsersController@register');
+//USer Login Route
+Route::post('/user-login','UsersController@login');
+//User LogOut Route
+Route::get('user-logout','UsersController@logout');
+
+//All Routes after login
+Route::group(['middleware' => ['frontlogin']],function(){
+//User account Page
+Route::match(['get','post'],'account','UsersController@account');
+//check user current pwd route
+Route::post('/check-user-pwd','UsersController@chkUserPassword');
+//update user password
+Route::post('/update-user-pwd','UsersController@updatePassword');
+//checkout page
+Route::match(['get','post'],'checkout','ProductsController@checkout');
+//Order Review Page
+Route::match(['get','post'],'order-review','ProductsController@orderReview');
+//Place Order
+Route::match(['get','post'],'place-order','ProductsController@placeOrder');
+//COD Thanks Page
+Route::get('/thanks','ProductsController@thanks');
+//Stripe  Page
+Route::get('/paypal','ProductsController@paypal');
+//User Orders Page
+Route::get('/orders','ProductsController@userOrders');
+//User Ordered Products Page
+Route::get('/orders/{id}','ProductsController@userOrderDetails');
+});
+
+//Check if user is already exists
+Route::match(['GET','POST'],'/check-email','UsersController@checkEmail');
+
+Route::group(['middleware' => ['auth']],function(){
+Route::get('/admin/dashboard', 'AdminController@dashboard');
+Route::get('/admin/settings', 'AdminController@settings');
+Route::get('/admin/check-pwd', 'AdminController@chkpassword');
+Route::match(['get','post'],'/admin/update-pwd','AdminController@updatePassword');
+
+// Categories Routes (Admin)
+Route::match(['get','post'],'/admin/add-category', 'CategoryController@addCategory');
+Route::match(['get','post'],'/admin/edit-category/{id}','CategoryController@editCategory');
+Route::match(['get','post'],'/admin/delete-category/{id}','CategoryController@deleteCategory');
+Route::get('/admin/view-categories', 'CategoryController@viewCategories');
+
+//Products Routes (Admin)
+Route::match(['get','post'],'/admin/add-product', 'ProductsController@addProduct');
+Route::match(['get','post'],'/admin/edit-product/{id}', 'ProductsController@editProduct');
+Route::get('/admin/view-products','ProductsController@viewProducts');
+Route::get('/admin/delete-product/{id}', 'ProductsController@deleteProduct');
+Route::get('/admin/delete-product-image/{id}','ProductsController@deleteProductImage');
+Route::get('/admin/delete-alt-image/{id}','ProductsController@deleteAltImage');
+
+//products attributes routes (Admin)
+Route::match(['get','post'],'/admin/add-attributes/{id}', 'ProductsController@addAttributes');
+Route::match(['get','post'],'/admin/edit-attributes/{id}', 'ProductsController@editAttributes');
+Route::match(['get','post'],'/admin/add-images/{id}', 'ProductsController@addImages');
+Route::get('/admin/delete-attribute/{id}', 'ProductsController@deleteAttribute');
+
+//Coupons Route
+Route::match(['get','post'],'/admin/add-coupon','CouponsController@addCoupon');
+Route::get('/admin/view-coupons', 'CouponsController@viewCoupons');
+Route::match(['get','post'],'/admin/edit-coupon/{id}','CouponsController@editCoupon');
+Route::get('/admin/delete-coupon/{id}','CouponsController@deleteCoupon');
+
+//Admin Orders Route
+Route::get('/admin/view-orders', 'ProductsController@viewOrders');
+
+//Admin Order Detail Page
+Route::get('/admin/view-order/{id}', 'ProductsController@viewOrderDetails');
+
+//Update Order Status
+Route::post('/admin/update-order-status','ProductsController@updateOrderStatus');
+});
+
+Route::get('/logout', 'AdminController@logout');
---
+++ storage/framework/views/1472669d36200553c40947453ecc01c1cdd96944.php
@@ -0,0 +1,190 @@
+<?php $__env->startSection('content'); ?>
+
+<!--main-container-part-->
+<div id="content">
+<div id="content-header">
+<div id="breadcrumb"> <a href="#" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a> <a href="#" class="current">Orders</a> </div>
+<h1>Oder #<?php echo e($orderDetails->id); ?></h1>
+<?php if(Session::has('flash_message_error')): ?>
+<div class="alert alert-error alert-block">
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+</button>
+<strong><?php echo e(session('flash_message_error')); ?></strong>
+</div>
+<?php endif; ?>
+<?php if(Session::has('flash_message_success')): ?>
+<div class="alert alert-success alert-block">
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+</button>
+<strong><?php echo e(session('flash_message_success')); ?></strong>
+</div>
+<?php endif; ?>
+</div>
+<div class="container-fluid">
+<hr>
+<div class="row-fluid">
+        <div class="span6">
+                <div class="widget-box">
+                <div class="widget-title">
+                    <h5>Order Details</h5>
+                </div>
+                <div class="widget-content nopadding">
+                    <table class="table table-striped table-bordered">
+                    
+            <tbody>
+                <tr>
+                <td class="taskDesc"> Order Date</td>
+                <td class="taskStatus"><?php echo e($orderDetails->created_at); ?></td>
+                </tr>
+                <tr>
+                <td class="taskDesc"> Order Status</td>
+                <td class="taskStatus"><?php echo e($orderDetails->order_status); ?></td>
+                </tr>
+                <tr>
+                <td class="taskDesc"> Order Total</td>
+                <td class="taskStatus">INR <?php echo e($orderDetails->grand_total); ?></td>
+                </tr>
+                <td class="taskDesc"> Shipping Charges</td>
+                <td class="taskStatus">INR <?php echo e($orderDetails->shipping_charges); ?></td>
+                </tr>
+                <tr>
+                <td class="taskDesc"> Coupon Code</td>
+                <td class="taskStatus"><?php echo e($orderDetails->coupon_code); ?></td>
+                </tr>
+                <td class="taskDesc"> Coupon Amount</td>
+                <td class="taskStatus">INR <?php echo e($orderDetails->coupon_amount); ?></td>
+                </tr>
+                <tr>
+                <td class="taskDesc"> Payment Method</td>
+                <td class="taskStatus"><?php echo e($orderDetails->payment_method); ?></td>
+                </tr>
+            </tbody>
+                    </table>
+                </div>
+                </div>
+                <div class="widget-box">
+                <div class="widget-title">
+                    <h5>Billing Address</h5>
+                </div>
+                <div class="widget-content">
+                    <?php echo e($userDetails->name); ?> <br>
+                    <?php echo e($userDetails->address); ?> <br>
+                    <?php echo e($userDetails->city); ?> <br>
+                    <?php echo e($userDetails->state); ?> <br>
+                    <?php echo e($userDetails->country); ?> <br>
+                    <?php echo e($userDetails->pincode); ?> <br>
+                    <?php echo e($userDetails->mobile); ?> <br>
+
+                </div>
+                </div>
+    </div>
+<div class="span6">
+        <div class="widget-box">
+                <div class="widget-title">
+                    <h5>Customer Details</h5>
+                </div>
+                <div class="widget-content nopadding">
+                    <table class="table table-striped table-bordered">
+                    
+            <tbody>
+                <tr>
+                <td class="taskDesc">Customer Name</td>
+                <td class="taskStatus"><?php echo e($orderDetails->name); ?></td>
+                </tr>
+                <tr>
+                <td class="taskDesc">Customer Email</td>
+                <td class="taskStatus"><?php echo e($orderDetails->user_email); ?></td>
+                </tr>
+            </tbody>
+                    </table>
+                </div>
+                </div>
+        <div class="widget-box">
+            <div class="widget-title">
+                <h5>Shipping Address Update</h5>
+            </div>
+            <div class="widget-content">
+            <form action="<?php echo e(url('/admin/update-order-status')); ?>" method="post"> <?php echo e(csrf_field()); ?>
+
+            <input type="hidden" name="order_id" value="<?php echo e($orderDetails->id); ?>">
+                   <table style="width:100%;">
+                    <tr>
+                    <td>
+                   <select name="order_status" id="order_status"  required="">
+                     <option value="New" <?php if($orderDetails->order_status== "New"): ?> selected <?php endif; ?>>
+                         New</option>
+                     <option value="Pending" <?php if($orderDetails->order_status== "Pending"): ?> selected <?php endif; ?>>
+                         Pending</option>
+                     <option value="In Process" <?php if($orderDetails->order_status== "In Process"): ?> selected <?php endif; ?>>
+                         In Process</option>
+                     <option value="Shipped" <?php if($orderDetails->order_status== "Shipped"): ?> selected <?php endif; ?>>
+                         Shipped</option>
+                     <option value="Delivered" <?php if($orderDetails->order_status== "Delivered"): ?> selected <?php endif; ?>>
+                         Delivered</option>
+                     <option value="Cancelled" <?php if($orderDetails->order_status== "Cancelled"): ?> selected <?php endif; ?>>
+                         Cancelled</option>
+                    <option value="Paid" <?php if($orderDetails->order_status== "Paid"): ?> selected <?php endif; ?>>
+                            Paid</option>
+                   </select>
+                    </td>
+                    <td>
+                   <input type="submit" value="Update Status" class="btn btn-info">
+                    </td>
+                    </tr>
+                   </table>
+               </form>
+            </div>
+        </div>
+        <div class="widget-box">
+        <div class="widget-title">
+            <h5>Shipping Address</h5>
+        </div>
+        <div class="widget-content">
+            <?php echo e($orderDetails->name); ?> <br>
+            <?php echo e($orderDetails->address); ?> <br>
+            <?php echo e($orderDetails->city); ?> <br>
+            <?php echo e($orderDetails->state); ?> <br>
+            <?php echo e($orderDetails->country); ?> <br>
+            <?php echo e($orderDetails->pincode); ?> <br>
+            <?php echo e($orderDetails->mobile); ?> <br>
+        </div>
+        </div>
+</div>
+</div>
+<hr>
+ <div class="row-fluid">
+     <h3 align="center">Ordered Products List</h3>
+    <table id="example" class="table table-striped table-bordered" style="width:100%">
+        <thead>
+            <tr>
+                <th>Product Code</th>
+                <th>Product Name</th>
+                <th>Product Size</th>
+                <th>Product Color</th>
+                <th>Product Price</th>
+                <th>Product Qty</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $__currentLoopData = $orderDetails->orders; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $pro): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <tr>
+            <td><?php echo e($pro->product_code); ?></td>
+                <td><?php echo e($pro->product_name); ?></td>
+                <td><?php echo e($pro->product_size); ?></td>
+                <td><?php echo e($pro->product_color); ?></td>
+                <td><?php echo e($pro->product_price); ?></td>
+                <td><?php echo e($pro->product_qty); ?></td>
+            </tr>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </tbody>
+    </table>
+
+ </div>
+</div>
+</div>
+<!--main-container-part-->
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.adminLayout.admin_design', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
